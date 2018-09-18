9d3816f675542ac2239088ea6c5859b0caff07a9
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2018-09-18 14:39:58 -0700

    Sheep Game

---
+++ Jump/Assets/Scenes/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float speed;
+    public float jumpPower;
+
+    private Rigidbody2D rbody;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rbody.velocity = new Vector2(speed, rbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpPower);
+        }
+    }
+}
